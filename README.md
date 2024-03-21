5bc297e2948f754a121c367d64095da9d5203914
shahbuland <smatiana1@gmail.com>
2024-03-21 11:18:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Latent Space Exploration
+
+You can find cool things moving between encoded prompts in a conditional diffusion models latent space. This repository is for doing that.
---
+++ fast_sd.py
@@ -0,0 +1,32 @@
+from diffusers import AutoencoderTiny
+from hacked_sdxl_pipeline import HackedSDXLPipeline
+import torch
+
+# Uncomment this if you want to compile
+from sfast.compilers.stable_diffusion_pipeline_compiler import (compile, CompilationConfig)
+
+def fast_diffusion_pipeline(model_id = "stabilityai/sdxl-turbo", vae_id = "madebyollin/taesdxl", compile = False):
+    """
+    :param compile: If true, does a bunch of stuff to make calls fast, but the first call will be very slow as a consequence
+        - If you use this, don't vary the batch size (probably)
+    """
+
+    pipe = HackedSDXLPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    pipe.vae = AutoencoderTiny.from_pretrained(vae_id, torch_dtype=torch.float16)
+
+    pipe.to('cuda')
+
+    if compile:
+        from sfast.compilers.stable_diffusion_pipeline_compiler import (compile, CompilationConfig)
+
+        config = CompilationConfig()
+        config.enable_jit = True
+        config.enable_jit_freeze = True
+        config.trace_scheduler = True
+        config.enable_cnn_optimization = True
+        config.preserve_parameters = False
+        config.prefer_lowp_gemm = True
+
+        pipe = compile(pipe, config)
+
+    return pipe
