6cd67fe32bc351aa7bdac9efaefa663e948033ec
shahbuland <smatiana1@gmail.com>
2024-03-31 20:34:46 -0400

    Weighting+Visualization

--- gradio_test.py
+++ gradio_test.py
@@ -7,22 +7,50 @@ from latent_interpolation import bezier, multi_lerp, partial_bezier
 
 explorer = LatentSpaceExplorer(compile = False)
 explorer.set_curve(bezier)
+
 def process_data(strings, slider_value):
-    prompts = strings.split(",")
-    prompts = [prompt.strip() for prompt in prompts]
+    try:
+        prompts = strings.split(",")
+        prompts = [prompt.strip() for prompt in prompts]
+        assert len(prompts) >= 2
+    except:
+        return None
 
     explorer.set_prompts(prompts)
-    return explorer.draw_sample(slider_value)
+    return explorer.draw_sample(slider_value), explorer.visualize(slider_value)
+
+def modify_weights(strings, t_slider_value, *slider_values):
+    weights = slider_values
+    try:
+        prompts = strings.split(",")
+        prompts = [prompt.strip() for prompt in prompts]
+        assert len(prompts) >= 2
+    except:
+        return None
+    weights = weights[:len(prompts)-2]
+
+    explorer.set_curve(partial_bezier(weights))
+    explorer.set_prompts(prompts)
+    return explorer.draw_sample(t_slider_value), explorer.visualize(t_slider_value)
+
+N_SLIDERS = 10
 
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column():
-            input_box = gr.Textbox(lines = 2)
-            slider = gr.Slider(0,1)
+            input_box = gr.Textbox(lines = 2, value = "a photo of a dog, a photo of a cat")
+            with gr.Row():
+                with gr.Column():
+                    slider = gr.Slider(0,1)
+                    vis = gr.Image()
+                with gr.Column():
+                    weight_sliders = [gr.Slider(0, 1, value = 0) for _ in range(N_SLIDERS)]
         with gr.Column():
             output_img = gr.Image()
 
-    slider.release(process_data, inputs = [input_box, slider], outputs = [output_img])
+    slider.release(process_data, inputs = [input_box, slider], outputs = [output_img, vis])
+    for i in range(N_SLIDERS):
+        weight_sliders[i].release(modify_weights, inputs = [input_box, slider] + weight_sliders, outputs = [output_img, vis])
 
-demo.launch(share = True)
+demo.launch()#share = True)
 
--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from latent_interpolation import bezier, multi_lerp, partial_bezier
 import torch
 import numpy as np
 from tqdm import tqdm
+from io import BytesIO
+import matplotlib.pyplot as plt
+import random
 
 class LatentSpaceExplorer:
     def __init__(self, compile = False):
@@ -14,6 +17,10 @@ class LatentSpaceExplorer:
         self.encodes = None
 
         self.curve = multi_lerp
+
+        # For visualization
+        self.prompts = None
+        self.stacked_points = None
     
     def fixed_seed(self, seed : int = 0):
         return torch.Generator('cuda').manual_seed(0)
@@ -30,6 +37,19 @@ class LatentSpaceExplorer:
         """
         assert len(prompts) > 1, "Need more than one prompt to explore"
 
+        def disjoint_uniform(a, b, c, d):
+            if random.random() <= 0.5:
+                return random.uniform(a, b)
+            else:
+                return random.uniform(c, d)
+
+        # For visualization
+        self.prompts = prompts
+        x_values = np.linspace(0, 1, len(prompts))
+        y_values = np.array([0 if i == 0 or i == len(x_values) - 1 else disjoint_uniform(-0.75, -0.25,0.25,0.75) for i in range(len(x_values))])
+
+        self.stacked_points = np.stack([x_values, y_values], axis = -1)
+
         self.encodes = self.pipe.get_encodes(prompts, generator = self.fixed_seed())
 
     def draw_sample(self, t):
@@ -44,6 +64,31 @@ class LatentSpaceExplorer:
         t_values = np.linspace(min_t, max_t, n_steps)
         samples = [self.draw_sample(t) for t in tqdm(t_values)]
         return samples
+
+    def visualize(self, t):
+        # Draw the curve
+        t_values = np.linspace(0, 1, 100)
+        points = np.stack([self.curve(self.stacked_points, t_i) for t_i in t_values])
+        plt.plot(points[:,0], points[:,1], color = 'blue')
+
+        # Draw the prompts
+        plt.scatter(self.stacked_points[:,0], self.stacked_points[:,1], color = 'blue')
+
+        crnt_point = self.curve(self.stacked_points, t)
+        plt.scatter(crnt_point[0], crnt_point[1], color = 'red')
+
+        for point, prompt in zip(self.stacked_points, self.prompts):
+            plt.text(point[0] + 0.01, point[1] + 0.01, prompt)
+        buffer = BytesIO()
+
+        plt.savefig(buffer, format = 'png')
+        buffer.seek(0)
+
+        res = Image.open(buffer)
+        plt.close()
+        return res
+
+
 
 if __name__ == "__main__":
     # Example showing how to use the model to explore the curve between "photograph of a dog" and "photograph of a cat", with the latent point "photograph of a car"
